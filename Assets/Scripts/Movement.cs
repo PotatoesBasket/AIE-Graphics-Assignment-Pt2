91c792a9a1c75db31f0318b2e7d78e20584ea899
PotatoesBasket <PotatoesBasket@hotmail.com>
2020-07-07 20:04:35 +1000

    Everything so far
    Because it's too much for my laptop now lol

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    float move = 0;
+    float turn = 0;
+    bool isRunning = false;
+    bool isJumping = false;
+
+    private void Update()
+    {
+        move = Input.GetAxis("Vertical");
+        turn = Input.GetAxis("Horizontal");
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+        isJumping = Input.GetKeyDown(KeyCode.Space);
+
+        animator.SetFloat("move", move);
+        animator.SetFloat("turn", turn);
+        animator.SetBool("isRunning", isRunning);
+
+        if (isJumping)
+            animator.SetTrigger("isJumping");
+    }
+}
